619b7df5f2816c481c61a0f533e96f95dce561ae
nikodem pankiewicz <nikodem.pankiewicz@hayden.ai>
2026-02-20 10:35:32 +0100

    build 0.1

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/nikodem",
+  assetPrefix: "/nikodem/",
 };
 
 export default nextConfig;
