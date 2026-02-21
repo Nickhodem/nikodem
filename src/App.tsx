661b0cfe3d6b0ad301f49b0a0be023620fa1a449
nikodem pankiewicz <nikodem.pankiewicz@hayden.ai>
2026-02-21 20:48:32 +0100

    update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/nikodem">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/ike-comparison" element={<IkeComparison />} />
